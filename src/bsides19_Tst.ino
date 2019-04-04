224d910b2b2a61bd7b97540f0f750b7c2683cbf7
burnsoft@gmail.com <burnsoft@gmail.com>
2019-04-03 21:33:27 -0500

    clean up

--- src/bsides19_Tst.ino
+++ src/bsides19_Tst.ino
@@ -175,45 +175,12 @@ void listNetworks() {
     while (true);
   }
 
-  // print the list of networks seen:
-  Serial.print("number of available networks:");
-  Serial.println(numSsid);
-
   // print the network number and name for each network found:
   for (int thisNet = 0; thisNet < numSsid; thisNet++) {
-    Serial.print(thisNet);
     if(WiFi.SSID(thisNet) == "batman and robin"){
       gameEnabled = 1;
       Serial.print("found batman and robin!");
       WifiFlag1 = 1;
     }
-    Serial.print(") ");
-    Serial.print(WiFi.SSID(thisNet));
-    Serial.print("\tSignal: ");
-    Serial.print(WiFi.RSSI(thisNet));
-    Serial.print(" dBm");
-    Serial.print("\tEncryption: ");
-    printEncryptionType(WiFi.encryptionType(thisNet));
-  }
-}
-
-void printEncryptionType(int thisType) {
-  // read the encryption type and print out the name:
-  switch (thisType) {
-    case ENC_TYPE_WEP:
-      Serial.println("WEP");
-      break;
-    case ENC_TYPE_TKIP:
-      Serial.println("WPA");
-      break;
-    case ENC_TYPE_CCMP:
-      Serial.println("WPA2");
-      break;
-    case ENC_TYPE_NONE:
-      Serial.println("None");
-      break;
-    case ENC_TYPE_AUTO:
-      Serial.println("Auto");
-      break;
   }
 }
